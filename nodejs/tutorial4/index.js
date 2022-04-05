0ab3aa404b6dd02107154bb2a55eaa0dee307865
https://github.com/ZaqoutMahmoud <mahmoud.zaqout@gmail.com>
2022-04-05 11:35:14 +0200

    update

--- nodejs/tutorial4/index.js
+++ nodejs/tutorial4/index.js
@@ -1,6 +1,27 @@
-// //dns core module
-// // google's ip address : 142.251.36.46
-// const dns = require("dns");
-// const w3 = dns.lookup("w3schools.com", function(err, addresses, family) {
-//   console.log(addresses);
+// filesystem in node.js [fs] > core module of nodejs
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+// read a file
+// fs.readFile('./readme.txt', 'utf8', (err, result) => {
+//     console.log(result)
 // });
+
+// write a file
+//let student = {id:1,name:'michaeljackson'}
+// fs.writeFile('dontreadme.txt', "This is a test", 'utf8', (err) => {
+//     console.log('saved')
+// })
+// fs.writeFile('student.json',JSON.stringify(student),'utf8',(err)=>{
+//     console.log('saved')
+// })
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    // here we need save that data.
+    fs.writeFile("posts.json", JSON.stringify(data), "utf8", (err) => {
+      console.log("Data is saved!");
+    });
+  });
